87060f87e6e0ffe4790c40a8aa7f9f5d61a786ca
Kin Hoang <kin.hoang@fwd.com>
2025-09-08 18:02:24 +0700

    dev

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
   res.send("Adaptive Eligibility POC running 🚀");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
